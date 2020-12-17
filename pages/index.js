35bf5f2ee231fcc79d142bbf4e53267883d406c2
furkan-cloud <firstinfirstout2020@gmail.com>
2020-12-17 23:04:03 +0300

    project template created

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Layout from "../components/layout/Layout";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <Layout>
+      <p> main layout</p>
+    </Layout>
+  );
+}
